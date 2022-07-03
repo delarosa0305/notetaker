09ab26c16ebae1093b39f6b874dd716ae17a091d
delarosa0305 <delarosa2381i@gmail.com>
2022-07-03 17:18:56 -0400

    setting up server listen

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { note } = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/', (req,res) => {
-    res.sendFile(path)
-})
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`API server now on ${PORT}`);
 });
+
+// http://localhost:3001/api/index
