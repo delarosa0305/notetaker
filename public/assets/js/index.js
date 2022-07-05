ccecd01b8286937e468e17b2b7e76841fb3c0e0c
delarosa0305 <delarosa2381i@gmail.com>
2022-07-05 19:39:02 -0400

    commit

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -23,7 +23,9 @@ const hide = (elem) => {
 };
 
 // activeNote is used to keep track of the note in the textarea
-let activeNote = {};
+let activeNote = {
+  
+};
 
 const getNotes = () =>
   fetch('/api/notes', {
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-const path = require('path');
-const express = require('express');
 const PORT = process.env.PORT || 3001;
+const express = require('express');
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () => {
-    console.log(`API server now on ${PORT}`);
+    console.log(`API server now on port ${PORT}!`);
 });
 
-// http://localhost:3001/api/index
+// http://localhost:3001/notes
